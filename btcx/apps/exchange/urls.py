91007bb3ea1ba4ef6e8333f2fc063214eb34f2e3
Evan Culver <evan@evanculver.com>
2013-06-16 22:26:23 -0700

    Change import to avoid deprecation warnings.

--- btcx/apps/exchange/urls.py
+++ btcx/apps/exchange/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url
 from btcx.apps.exchange.views import HomeView, BuyApiView, SellApiView
 
 urlpatterns = patterns('',
